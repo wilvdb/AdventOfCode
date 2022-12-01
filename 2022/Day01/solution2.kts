437a54bc3eaffe017d2d587ba92aa9966599522b
Wilfried Vandenberghe <wilfried.vandenberghe@ctie.etat.lu>
2022-12-01 08:59:18 +0100

    refactor

--- 2022/Day01/solution2.kts
+++ 2022/Day01/solution2.kts
@@ -2,26 +2,27 @@ import java.io.File
 
 val lines = File("input2.txt").readLinesAsInt()
 
-println("Result : ${sumUpTop(aggregate(lines))}")
+println("Result : ${aggregate(lines).topN(3)}")
 
-fun sumUpTop(input: List<Int>) = input.sorted().reversed().subList(0, 3).sum()
+fun aggregate(input: List<Int?>) = input.splitBy { it == null }.map { it.sum() }
 
-
-fun aggregate(input: List<Int?>): List<Int> {
-    val result = ArrayList<Int>()
-    var currentIndex = 0
-    result.add(0)
-    for (el in input) {
-        if(el == null) {
-            currentIndex++
-            result.add(0)
+fun List<Int?>.splitBy(predicate: (Int?) -> Boolean):List<List<Int>> {
+    val result = ArrayList<List<Int>>()
+    var currentList = ArrayList<Int>()
+    result.add(currentList)
+    for (el in this) {
+        if(predicate.invoke(el)) {
+            currentList = ArrayList()
+            result.add(currentList)
             continue
         }
 
-        result[currentIndex] += el
+        currentList.add(el?:0)
     }
 
     return result
 }
 
 fun File.readLinesAsInt(): List<Int?>  = this.readLines().map { if (it.isEmpty()) null else it.toInt() }
+
+fun List<Int>.topN(n: Int): Int = this.sorted().reversed().subList(0, n).sum()

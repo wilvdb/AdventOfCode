efb4283f6ec30ff7e085616efa5b895bcc7fefa2
Wilfried Vandenberghe <wilfried.vandenberghe@ctie.etat.lu>
2022-12-02 09:10:23 +0100

    Day 2

--- 2022/Day02/solution1.kts
+++ 2022/Day02/solution1.kts
@@ -1,8 +1,40 @@
 import java.io.File
 
-val lines = File("input1.txt").readLinesAsInt()
 
-println("Result : ${lines}")
+val challenge = File("input1.txt").readLinesAsInt()
 
 
-fun File.readLinesAsInt(): List<Int?>  = this.readLines().map { if (it.isEmpty()) null else it.toInt() }
+println("Result : ${challenge.sumOf { it.getPoints() }}")
+
+fun File.readLinesAsInt(): List<Round>  = this.readLines().map { Round(it.substring(0, 1), it.substring(2, 3)) }
+
+class Round(val elve: String, val me: String) {
+
+    private fun getElvePoints() = getPoints(elve)
+
+    private fun getMyPoints() = getPoints(me)
+    fun getPoints() = when {
+        isDraw()-> 3 + getMyPoints()
+        isWin() -> 6 + getMyPoints()
+        else -> getMyPoints()
+    }
+
+    private fun getPoints(letter: String) = when(letter) {
+        "A" -> 1
+        "X" -> 1
+        "B" -> 2
+        "Y" -> 2
+        "C" -> 3
+        "Z" -> 3
+        else -> 0
+    }
+
+    private fun isWin() = when {
+        getElvePoints() == 1 && getMyPoints() == 2 -> true
+        getElvePoints() == 2 && getMyPoints() == 3 -> true
+        getElvePoints() == 3 && getMyPoints() == 1 -> true
+        else -> false
+    }
+
+    private fun isDraw() = getElvePoints() == getMyPoints()
+}
--- 2022/Day02/solution2.kts
+++ 2022/Day02/solution2.kts
@@ -1,8 +1,29 @@
 import java.io.File
 
-val lines = File("input2.txt").readLinesAsInt()
 
-println("Result : ${lines}")
+val challenge = File("input2.txt").readLinesAsInt()
 
 
-fun File.readLinesAsInt(): List<Int?>  = this.readLines().map { if (it.isEmpty()) null else it.toInt() }
+println("Result : ${challenge.sumOf { it.getPoints() }}")
+
+fun File.readLinesAsInt(): List<Round>  = this.readLines().map { Round(it.substring(0, 1), it.substring(2, 3)) }
+
+class Round(val elve: String, val me: String) {
+
+    private fun getElvePoints() =  when(elve) {
+        "A" -> 1
+        "B" -> 2
+        "C" -> 3
+        else -> 0
+    }
+
+    fun getPoints() = when {
+        isDraw() -> 3 + getElvePoints()
+        isWin() -> 6 + if (getElvePoints() + 1 == 4)  1 else getElvePoints() + 1
+        else -> if (getElvePoints() - 1 == 0)  3 else getElvePoints() - 1
+    }
+
+    private fun isWin() = me == "Z"
+
+    private fun isDraw() = me == "Y"
+}

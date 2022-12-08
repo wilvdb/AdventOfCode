5c0efe2ada6ece837f7e9f15f6d9378b66a6aec7
Wilfried Vandenberghe <wilfried.vandenberghe@ctie.etat.lu>
2022-12-08 17:06:27 +0100

    completed day 8 part 1

---
+++ 2022/Day08/solution1.kts
@@ -0,0 +1,60 @@
+import java.io.File
+
+
+val trees = File("input1.txt").readLinesAs { it.map { c -> c.digitToInt() } }
+
+
+println("Result : ${countVisibleTrees(trees)}")
+
+fun <T> File.readLinesAs(function: (String) -> T): List<T>  = this.readLines().map { function.invoke(it) }
+
+fun countVisibleTrees(trees: List<List<Int>>): Int {
+    var count = 0
+  for(y in 0 until trees.size) {
+      for(x in 0 until trees[y].size) {
+          if(isVisible(trees, x, y)) {
+              count++
+          }
+      }
+  }
+
+    return count;
+}
+
+fun isVisible(trees: List<List<Int>>, x: Int, y: Int): Boolean {
+    var size = trees[y][x]
+
+    var leftVisible = true;
+    for(left in 0 until x) {
+        if(trees[y][left] >= size) {
+            leftVisible = false;
+            break;
+        }
+    }
+
+    var rightVisible = true;
+    for(right in x + 1 until trees[y].size) {
+        if(trees[y][right] >= size) {
+            rightVisible = false;
+            break;
+        }
+    }
+
+    var topVisible = true;
+    for(top in 0 until y) {
+        if(trees[top][x] >= size) {
+            topVisible = false;
+            break;
+        }
+    }
+
+    var bottomVisible = true;
+    for(bottom in y + 1 until trees.size) {
+        if(trees[bottom][x] >= size) {
+            bottomVisible = false;
+            break;
+        }
+    }
+
+    return rightVisible || leftVisible || topVisible || bottomVisible;
+}

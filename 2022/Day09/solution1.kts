43eced8ac7dfce6439de5366bff70b25e50930dc
Wilfried Vandenberghe <wilfried.vandenberghe@ctie.etat.lu>
2022-12-15 08:30:34 +0100

    refactor for using typealias

--- 2022/Day09/solution1.kts
+++ 2022/Day09/solution1.kts
@@ -1,11 +1,12 @@
 import java.io.File
 import kotlin.math.abs
 
+typealias Knot = Pair<Int, Int>
+
 val motions = File("input1.txt").readLinesAs { Motion(it.split(" ")[0], it.split(" ")[1].toInt()) }
 
 fun <T> File.readLinesAs(function: (String) -> T): List<T>  = this.readLines().map { function.invoke(it) }
 
-var points = listOf<Pair<Int, Int>>(Pair(0, 0))
 
 val rope = Rope()
 
@@ -14,11 +15,11 @@ class Motion(val direction: String, val steps: Int)
 
 class Rope {
 
-    var head: Pair<Int, Int> = Pair(10_000, 10_000)
-    var tail: Pair<Int, Int> = Pair(10_000, 10_000)
+    var head: Knot = Knot(10_000, 10_000)
+    var tail: Knot = Knot(10_000, 10_000)
 
-    fun moveHead(motion: Motion): Set<Pair<Int, Int>> {
-        var result = setOf<Pair<Int, Int>>(tail)
+    fun moveHead(motion: Motion): Set<Knot> {
+        var result = setOf(tail)
         for(step in 1..motion.steps) {
             head = when(motion.direction) {
                 "R" -> Pair(head.first + 1, head.second)
@@ -34,32 +35,33 @@ class Rope {
         return result
     }
 
-    private fun moveTail(): Pair<Int, Int> {
-        if(needToMove()) {
+    private fun moveTail(): Knot {
+        if(head.needToMove(tail)) {
             if(tail.first == head.first) {
-                if(tail.second < head.second) {
-                    return Pair<Int, Int>(tail.first, tail.second + 1)
+                return if(tail.second < head.second) {
+                    tail.copy(second = tail.second + 1)
                 } else {
-                    return Pair<Int, Int>(tail.first, tail.second - 1)
+                    tail.copy(second = tail.second - 1)
                 }
             }
 
             if(tail.second == head.second) {
-                if(tail.first < head.first) {
-                    return Pair<Int, Int>(tail.first + 1, tail.second)
+                return if(tail.first < head.first) {
+                    tail.copy(first = tail.first + 1)
                 } else {
-                    return Pair<Int, Int>(tail.first - 1, tail.second)
+                    tail.copy(first = tail.first - 1, tail.second)
                 }
             }
 
             val x = if(tail.first < head.first) { tail.first + 1 } else { tail.first - 1 }
             val y = if(tail.second < head.second) { tail.second + 1 } else { tail.second - 1 }
 
-            return Pair(x, y)
+            return Knot(x, y)
         }
 
         return tail
     }
 
-    private fun needToMove() = abs(head.first - tail.first) > 1 || abs(head.second - tail.second) > 1
 }
+
+fun Knot.needToMove(knot: Knot) = abs(this.first - knot.first) > 1 || abs(this.second - knot.second) > 1
